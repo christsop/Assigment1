55fb3ab37986e97bc2760eed8dc8e05046232cf2
christos <p3130211@dias.aueb.gr>
2019-01-06 15:33:42 +0200

    7 commit

--- Array.h
+++ Array.h
@@ -1,123 +1,150 @@
 #ifndef ASS1PROJECT_ARRAY_H
 #define ASS1PROJECT_ARRAY_H
 
-#include "Color.h"
+
 #include <string>
-#include "ppm/ppm.h"
+#include <iostream>
 
-/*! The imaging namespace contains every class or function associated with the image storage, compression and manipulation.
- */
-namespace math
+using namespace std;
+
+template <typename T>
+
+class Array
 {
+public:
 
+protected:
+    T * buffer;                              //! Holds the image data. pointer buffer is pointing to array of Color's
 
-    template <class T>
-    class Array
-            {
-    public:
+    unsigned int width, 						 //! The width of the image (in pixels)
+            height;		                 //! The height of the image (in pixels)
 
-    protected:
-        std::vector<T> * buffer;                              //! Holds the image data. pointer buffer is pointing to array of Color's
+public:
+    // metric accessors
 
-        unsigned int width, 						 //! The width of the image (in pixels)
-                height;		                 //! The height of the image (in pixels)
+    /*! Returns the width of the image
+     */
+    const unsigned int getWidth() const {return width;}
 
-    public:
-        // metric accessors
+    /*! Returns the height of the image
+     */
+    const unsigned int getHeight() const {return height;}
 
-        /*! Returns the width of the image
-         */
-        const unsigned int getWidth() const {return width;}
+    // data accessors
 
-        /*! Returns the height of the image
-         */
-        const unsigned int getHeight() const {return height;}
+    /*! Obtains a pointer to the internal data.
+     *
+     *  This is NOT a copy of the internal image data, but rather a pointer
+     *  to the internally allocated space, so DO NOT attempt to delete the pointer.
+     */
+    T * getRawDataPtr();
 
-        // data accessors
+    /*! Obtains the color of the image at location (x,y).
+     *
+     *  The method should do any necessary bounds checking.
+     *
+     *  \param x is the (zero-based) horizontal index of the pixel to get.
+     *  \param y is the (zero-based) vertical index of the pixel to get.
+     *
+     *  \return The color of the (x,y) pixel as a Color object. Returns a black (0,0,0) color in case of an out-of-bounds x,y pair.
+     */
+    T getPixel(unsigned int x, unsigned int y) const;
 
-        /*! Obtains a pointer to the internal data.
-         *
-         *  This is NOT a copy of the internal image data, but rather a pointer
-         *  to the internally allocated space, so DO NOT attempt to delete the pointer.
-         */
-        Array * getRawDataPtr();
+    // data mutators
 
-        /*! Obtains the color of the image at location (x,y).
-         *
-         *  The method should do any necessary bounds checking.
-         *
-         *  \param x is the (zero-based) horizontal index of the pixel to get.
-         *  \param y is the (zero-based) vertical index of the pixel to get.
-         *
-         *  \return The color of the (x,y) pixel as a Color object. Returns a black (0,0,0) color in case of an out-of-bounds x,y pair.
-         */
-        Array getPixel(unsigned int x, unsigned int y) const;
+    /*! Sets the RGB values for an (x,y) pixel.
+     *
+     *  The method should perform any necessary bounds checking.
+     *
+     *  \param x is the (zero-based) horizontal index of the pixel to set.
+     *  \param y is the (zero-based) vertical index of the pixel to set.
+     *  \param value is the new color for the (x,y) pixel.
+     */
+    void setPixel(unsigned int x, unsigned int y, T & value);
 
-        // data mutators
+    /*! Copies the image data from an external raw buffer to the internal image buffer.
+     *
+     *  The member function ASSUMES that the input buffer is of a size compatible with the internal storage of the
+     *  Image object and that the data buffer has been already allocated. If the image buffer is not allocated or the
+     *  width or height of the image are 0, the method should exit immediately.
+     *
+     *  \param data_ptr is the reference to the preallocated buffer from where to copy the data to the Image object.
+     */
+    void setData(const T * & data_ptr);
 
-        /*! Sets the RGB values for an (x,y) pixel.
-         *
-         *  The method should perform any necessary bounds checking.
-         *
-         *  \param x is the (zero-based) horizontal index of the pixel to set.
-         *  \param y is the (zero-based) vertical index of the pixel to set.
-         *  \param value is the new color for the (x,y) pixel.
-         */
-        void setPixel(unsigned int x, unsigned int y, Array & value);
+    // constructors and destructor
 
-        /*! Copies the image data from an external raw buffer to the internal image buffer.
-         *
-         *  The member function ASSUMES that the input buffer is of a size compatible with the internal storage of the
-         *  Image object and that the data buffer has been already allocated. If the image buffer is not allocated or the
-         *  width or height of the image are 0, the method should exit immediately.
-         *
-         *  \param data_ptr is the reference to the preallocated buffer from where to copy the data to the Image object.
-         */
-        void setData(const Array * & data_ptr);
+    /*! Default constructor.
+     *
+     * By default, the dimensions of the image should be zero and the buffer must be set to nullptr.
+     */
+    Array();
 
-        // constructors and destructor
+    /*! Constructor with width and height specification.
+     *
+     * \param width is the desired width of the new image.
+     * \param height is the desired height of the new image.
+     */
+    Array(unsigned int width, unsigned int height);
 
-        /*! Default constructor.
-         *
-         * By default, the dimensions of the image should be zero and the buffer must be set to nullptr.
-         */
-        Array();
+    /*! Constructor with data initialization.
+     *
+     * \param width is the desired width of the new image.
+     * \param height is the desired height of the new image.
+     * \param data_ptr is the source of the data to copy to the internal image buffer.
+     *
+     * \see setData
+     */
+    Array(unsigned int width, unsigned int height, const T * data_ptr);
 
-        /*! Constructor with width and height specification.
-         *
-         * \param width is the desired width of the new image.
-         * \param height is the desired height of the new image.
-         */
-        Array(unsigned int width, unsigned int height);
+    /*! Copy constructor.
+     *
+     * \param src is the source image to replicate in this object.
+     */
+    Array(const Array &src);
 
-        /*! Constructor with data initialization.
-         *
-         * \param width is the desired width of the new image.
-         * \param height is the desired height of the new image.
-         * \param data_ptr is the source of the data to copy to the internal image buffer.
-         *
-         * \see setData
-         */
-        Array(unsigned int width, unsigned int height, const std::vector<T> * data_ptr);
+    /*! The Image destructor.
+     */
+    ~Array();
 
-        /*! Copy constructor.
-         *
-         * \param src is the source image to replicate in this object.
-         */
-        Array(const Array &src);
+    /*! Copy assignment operator.
+     *
+     * \param right is the source image.
+     */
+    Array & operator = (const Array & right);
 
-        /*! The Image destructor.
-         */
-        ~Array();
+    T & operator ()(int i, int j){
 
-        /*! Copy assignment operator.
-         *
-         * \param right is the source image.
-         */
-        Array & operator = (const Array & right);
-
+        return buffer[i*width+j];
     };
 
-} //namespace imaging
+};
+
+template <typename T>
+Array<T>::Array() {
+    width = 0;
+    height = 0;
+    std::vector<T> * buffer = nullptr;
+}
+
+template <typename T>
+Array<T>::Array(unsigned int w, unsigned int h) :width(w), height(h) {
+
+}
+
+//constructor with image data
+template <typename T>
+Array<T>::Array(unsigned int w, unsigned int h, const T *data)  :width(w), height(h), buffer(const_cast<T*>(data)) {}
+
+//// copy constructor
+template <typename T>
+Array<T>::Array(const Array &src) :width(src.width), height(src.height), buffer(const_cast<T*>(src.buffer)) {
+
+}
+
+////deconstructor
+template <typename T>
+Array<T>::~Array() {}
+
 
 #endif
--- main.cpp
+++ main.cpp
@@ -2,75 +2,56 @@
 #include <string>
 #include "ppm/ppm.h"
 #include "Image.h"
+#include "Array.h"
 
 #include <iostream>
 #include <fstream>
 
 using namespace imaging;
 using namespace std;
-
-
-template <class myType, class myType1>
-myType GetMax (myType a, myType1 b) {
-    myType1 toReturn = a > b ? a : b;
-    return toReturn;
-}
-
-template<class Wtype, class Htype, class INtype>
-class myImage {
-public:INtype iname;
-public:Wtype iwidth;
-public:Htype iheight;
-
-public:
-    myImage (Wtype width, Htype height, INtype name) {
-        iwidth = width;
-        iheight = height;
-        iname = name;
-    }
-};
+//using namespace math;
 
 int main(int argc, char *argv[]) {
+
 
     string n = "sample";
     int h = 1080;
     int w = 1920;
 
-    myImage<int, int, string> image (w, h, n);
+    //myImage<int, int, string> image (w, h, n);
 
-    cout << image.iwidth << endl;
+    char *filename = new char[15];
+
+    if(argc == 1) {
+        cout << "File name of the image to load: ";
+        cin >> filename;
+    } else if (argc == 2) {
+        filename = argv[1];
+    } else {
+        return 0;
+    }
 
 
+    int width = 0;
+    int height = 0;
+
+    float * imageData = ReadPPM(filename, &width, &height);
+
+    Color * imageColorData = new Color[width*height];
+
+    int j = 0;
+    for(int i=0; i<width*height*3; i = i+3) {
+        imageColorData[j++] = Color(imageData[i], imageData[i+1], imageData[i+2]);
+    }
+
+    Array<Color> imageArray(width, height, imageColorData);
 
 
-//
-//    char *filename = new char[15];
-//
-//    if(argc == 1) {
-//        cout << "File name of the image to load: ";
-//        cin >> filename;
-//    } else if (argc == 2) {
-//        filename = argv[1];
-//    } else {
-//        return 0;
-//    }
-//
-//
-//    int width = 0;
-//    int height = 0;
-//
-//    float * imageData = ReadPPM(filename, &width, &height);
-//
-//    Color * imageColorData = new Color[width*height];
-//
-//    int j = 0;
-//    for(int i=0; i<width*height*3; i = i+3) {
-//        imageColorData[j++] = Color(imageData[i], imageData[i+1], imageData[i+2]);
-//    }
-//
-//    Image myImage(width, height, imageColorData);
-//
-//    bool status = WritePPM(imageData,width, height, "negoriginal.ppm");
+    Image myImage(width, height, imageColorData);
+
+    cout << &imageArray(1,2) << endl;
+
+    bool status = WritePPM(imageData,width, height, "negoriginal.ppm");
 
 
 //    system("pause");
